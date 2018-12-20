f1ba7a4363ba04b289c64d4430cc6c07d2195861
huangzhongxi <huangzhongxi@yundun.com>
2018-12-20 17:24:47 +0800

    个人中心001

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name') }}</title>
+    <link rel="shortcut icon" href="{{ config('blog.default_icon') }}">
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
